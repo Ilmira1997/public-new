ae1bdf1c68e8103a2e2ac0fa8e82e58bd111afa7
Ilmira1997 <164889576+Ilmira1997@users.noreply.github.com>
2024-06-18 00:39:34 -0700

    public

--- Курс 50 Фронтенд/Convertor/main.js
+++
@@ -1,6 +0,0 @@
-function conventorCrnc() {
-    let USD = Number(document.getElementsByName("x")[0].value);
-    let result = USD * 12450;
-    let UZB = document.getElementsByName("result")[0]
-    return UZB.value = result;
-}
